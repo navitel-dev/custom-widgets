bf6e37b0efcb8b4aadde75b8c8a5d3b01d80146c
Nikita Kolesnikov <nkolesnikov@ac-sw.com>
2023-03-15 18:06:09 +0700

    Clear code

--- lib/container/decorated_container.dart
+++ lib/container/decorated_container.dart
@@ -30,45 +30,23 @@ class DecoratedBox extends SingleChildRenderObjectWidget {
       ..configuration = createLocalImageConfiguration(context)
       ..position = position;
   }
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    final String label;
-    switch (position) {
-      case DecorationPosition.background:
-        label = 'bg';
-        break;
-      case DecorationPosition.foreground:
-        label = 'fg';
-        break;
-    }
-    properties.add(EnumProperty<DecorationPosition>('position', position,
-        level: DiagnosticLevel.hidden));
-    properties.add(DiagnosticsProperty<Decoration>(label, decoration));
-  }
 }
 
 class DecoratedContainer extends StatelessWidget {
   DecoratedContainer({
     super.key,
     this.alignment,
-    this.padding,
     this.color,
-    this.foregroundDecoration,
     required this.radius,
     double? width,
     double? height,
     BoxConstraints? constraints,
-    this.margin,
     this.transform,
     this.transformAlignment,
     this.child,
     this.clipBehavior = Clip.none,
     this.decoration,
-  })  : assert(margin == null || margin.isNonNegative),
-        assert(padding == null || padding.isNonNegative),
-        assert(constraints == null || constraints.debugAssertIsValid()),
+  })  : assert(constraints == null || constraints.debugAssertIsValid()),
         assert(clipBehavior == Clip.none),
         constraints = (width != null || height != null)
             ? constraints?.tighten(width: width, height: height) ??
@@ -77,26 +55,12 @@ class DecoratedContainer extends StatelessWidget {
   final double radius;
   final Widget? child;
   final AlignmentGeometry? alignment;
-  final EdgeInsetsGeometry? padding;
   final Color? color;
   final Decoration? decoration;
-  final Decoration? foregroundDecoration;
   final BoxConstraints? constraints;
-  final EdgeInsetsGeometry? margin;
   final Matrix4? transform;
   final AlignmentGeometry? transformAlignment;
   final Clip clipBehavior;
-
-  EdgeInsetsGeometry? get _paddingIncludingDecoration {
-    if (decoration == null || decoration!.padding == null) {
-      return padding;
-    }
-    final EdgeInsetsGeometry? decorationPadding = decoration!.padding;
-    if (padding == null) {
-      return decorationPadding;
-    }
-    return padding!.add(decorationPadding!);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -110,11 +74,6 @@ class DecoratedContainer extends StatelessWidget {
       );
     } else if (alignment != null) {
       current = Align(alignment: alignment!, child: current);
-    }
-
-    final EdgeInsetsGeometry? effectivePadding = _paddingIncludingDecoration;
-    if (effectivePadding != null) {
-      current = Padding(padding: effectivePadding, child: current);
     }
 
     final decoration = BoxDecoration(
@@ -148,20 +107,8 @@ class DecoratedContainer extends StatelessWidget {
 
     current = DecoratedBox(decoration: decoration, child: current);
 
-    if (foregroundDecoration != null) {
-      current = DecoratedBox(
-        decoration: foregroundDecoration!,
-        position: DecorationPosition.foreground,
-        child: current,
-      );
-    }
-
     if (constraints != null) {
       current = ConstrainedBox(constraints: constraints!, child: current);
-    }
-
-    if (margin != null) {
-      current = Padding(padding: margin!, child: current);
     }
 
     if (transform != null) {
